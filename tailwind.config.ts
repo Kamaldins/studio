f495280bd22afd8200249ae31047282f273084b6
39ab <frogeet6@gmail.com>
2025-11-01 18:21:42 +0000

    change the website font to something from google fonts, this is just ugl

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,8 +17,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['"PT Sans"', 'sans-serif'],
-        headline: ['"Playfair Display"', 'serif'],
+        body: ['"Inter"', 'sans-serif'],
+        headline: ['"Poppins"', 'sans-serif'],
       },
       colors: {
         border: 'hsl(var(--border))',
