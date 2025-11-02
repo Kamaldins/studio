1ac4cd9b0cb44ca3b15547091024509dc680472b
39ab <frogeet6@gmail.com>
2025-11-02 00:28:04 +0000

    im telling you to change the full code for the translations, make them d

--- src/lib/get-dictionary.ts
+++ src/lib/get-dictionary.ts
@@ -4,10 +4,10 @@ import type { Locale } from '@/i18n-config';
 
 // We use a Record to type the dictionaries to allow for dynamic access
 const dictionaries: Record<Locale, () => Promise<any>> = {
-  en: () => import('@/dictionaries/en').then((module) => module.dictionary),
-  de: () => import('@/dictionaries/de').then((module) => module.dictionary),
-  lv: () => import('@/dictionaries/lv').then((module) => module.dictionary),
-  ru: () => import('@/dictionaries/ru').then((module) => module.dictionary),
+  en: () => import('@/dictionaries/en.json').then((module) => module.default),
+  de: () => import('@/dictionaries/de.json').then((module) => module.default),
+  lv: () => import('@/dictionaries/lv.json').then((module) => module.default),
+  ru: () => import('@/dictionaries/ru.json').then((module) => module.default),
 };
 
 export const getDictionary = async (locale: Locale) => {
