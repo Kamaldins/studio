f80bfd8ed60911ce2c9c616866856e423092aa8e
39ab <frogeet6@gmail.com>
2025-11-02 13:13:20 +0000

    you have to understand the fact its all link based, nothings hosted anyw

--- src/app/[lang]/sauna/page.jsx
+++ src/app/[lang]/sauna/page.jsx
@@ -5,7 +5,8 @@ import { getDictionary } from '@/lib/get-dictionary';
 import { PlaceHolderImages } from '@/lib/placeholder-images';
 import { Check } from 'lucide-react';
 
-export default async function SaunaPage({ params: { lang } }) {
+export default async function SaunaPage({ params }) {
+    const { lang } = params;
   const dictionary = await getDictionary(lang);
   const saunaImages = PlaceHolderImages.filter(p => p.category === 'sauna');
 
