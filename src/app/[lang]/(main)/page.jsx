258aa1f94f8e131ec53077b56258a65b7ba0b031
39ab <frogeet6@gmail.com>
2025-11-02 14:23:40 +0000

    ok no scrap that, this is nonworking, lets try another way, prepare ever

--- src/app/[lang]/(main)/page.jsx
+++ src/app/[lang]/(main)/page.jsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 import { getDictionary } from '@/lib/get-dictionary';
 import { PlaceHolderImages } from '@/lib/placeholder-images';
 import { i18n } from '@/i18n-config';
-import PageContent from './page-content';
+import PageContent from '../page-content';
 
 export default async function Page({ params }) {
   const lang = params.lang && i18n.locales.includes(params.lang) ? params.lang : i18n.defaultLocale;
@@ -17,4 +17,3 @@ export default async function Page({ params }) {
     />
   );
 }
-
