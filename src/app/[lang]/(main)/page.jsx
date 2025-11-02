e499f3ea10bf1bfd553fec28056b8db7aa62cc35
39ab <frogeet6@gmail.com>
2025-11-02 14:09:54 +0000

    Try fixing this error: `Build Error: Ecmascript file had an error. Error

--- src/app/[lang]/(main)/page.jsx
+++ src/app/[lang]/(main)/page.jsx
@@ -1,13 +1,20 @@
+
 import * as React from 'react';
 import { getDictionary } from '@/lib/get-dictionary';
 import { PlaceHolderImages } from '@/lib/placeholder-images';
-import PageContent from '../page-content';
 import { i18n } from '@/i18n-config';
+import PageContent from './page-content';
 
 export default async function Page({ params }) {
   const lang = params.lang && i18n.locales.includes(params.lang) ? params.lang : i18n.defaultLocale;
   const dictionary = await getDictionary(lang);
   const imageUrls = PlaceHolderImages.map(p => p.imageUrl);
-
-  return <PageContent dictionary={dictionary} imageUrls={imageUrls} />;
+  
+  return (
+    <PageContent 
+      dictionary={dictionary} 
+      imageUrls={imageUrls}
+    />
+  );
 }
+
